8d264f1d0139a538d0dac1984776fa3f3ebf825e
Szymon Żak <szymon.zak55@gmail.com>
2022-09-08 21:55:05 +0200

    fix(users): store action

--- app/Modules/v1/Users/Requests/UserRequest.php
+++ app/Modules/v1/Users/Requests/UserRequest.php
@@ -7,19 +7,17 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
-class UpdateUserRequest extends FormRequest
+class UserRequest extends FormRequest
 {
-	public function authorize(): bool
-	{
-		return Gate::allows('users_update');
-	}
-
 	public function rules(): array
 	{
 		return [
 			//todo: some more strict rules? or admin should do anything?
-			'name' => ['sometimes'],
-			'role' => ['sometimes', Rule::in(array_column(Role::cases(), 'value'))]
+			//fixme: reuse rules from registration?
+			'name' => ['required'],
+			'email' => ['required', 'email'],
+			'role' => ['required', Rule::in(array_column(Role::cases(), 'value'))],
+			'password' => ['required'],
 		];
 	}
 }
